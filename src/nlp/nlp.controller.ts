e8ea342f4e32871e8c1e1603d6ccbb842d81b25a
Abhay Sharma <surferab368@gmail.com>
2024-01-18 17:37:35 +0530

    maxlife_data_training_retrieval

--- src/nlp/nlp.controller.ts
+++ src/nlp/nlp.controller.ts
@@ -3,7 +3,8 @@ import { NlpService } from './nlp.service';
 import { Get } from '@nestjs/common/decorators/http/request-mapping.decorator';
 import { mergeScan } from 'rxjs';
 import { Param } from '@nestjs/common/decorators/http/route-params.decorator';
-
+import { Client } from 'pg';
+import * as fs from 'fs';
 @Controller('nlp')
 export class NlpController {
   constructor(private readonly nlpService: NlpService) {}
@@ -17,4 +18,26 @@ export class NlpController {
   public async processIntent(@Param('message') msg): Promise<string> {
     return this.nlpService.processIntent(msg);
   }
+//   @Get('getdbdata')
+//   public async getData(){
+
+//     const client = new Client({
+//         username: 'postgres',
+//         host: 'postgres.gcp.corover.ai',
+//         database: 'postgres',
+//         password: 'PVc)4tWX$oMB',
+//         port: 5432,
+//       });
+      
+//       await client.connect();
+
+//       const res = await client.query("SELECT * FROM knowledge_base WHERE appId='5cb1ef3f-d380-45af-9ccc-a3a755afb92a' LIMIT 20;");
+
+//       const data = JSON.stringify(res.rows);
+//       fs.writeFileSync('data.json', data);
+//       await client.end();
+
+
+
+//   }
 }
--- src/nlp/nlp.service.ts
+++ src/nlp/nlp.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { NlpManager } from 'node-nlp';
+import * as csv from 'csv-parser';
 import * as fs from 'fs';
 
 
@@ -12,15 +13,20 @@ export class NlpService {
   }
 
   public async trainModel(): Promise<void> {
-    const data = JSON.parse(fs.readFileSync('./trainingData.json', 'utf8'));
-    for (const intent of data.intents) {
-      for (const utterance of intent.utterances) {
-        this.manager.addDocument('en', utterance, intent.intent);
-      }
-      this.manager.addAnswer('en', intent.intent, intent.answer);
-    }
-    await this.manager.train();
-    this.manager.save();
+    const data: any[] = [];
+    fs.createReadStream('./trainingData.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        data.push(row);
+      })
+      .on('end', () => {
+        for (const intent of data) {
+          this.manager.addDocument('en', intent.utterances, intent.intent);
+          this.manager.addAnswer('en', intent.intent, intent.answers);
+        }
+        this.manager.train();
+        this.manager.save();
+      });
   }
 
   public async processIntent(text: string): Promise<string> {
