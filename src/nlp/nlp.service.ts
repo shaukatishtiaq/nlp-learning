578626101b4942fc9558053df5fe109e68188917
Abhay Sharma <surferab368@gmail.com>
2024-01-18 17:58:39 +0530

    embeddings_update

--- src/nlp/nlp.service.ts
+++ src/nlp/nlp.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { NlpManager } from 'node-nlp';
 import * as csv from 'csv-parser';
 import * as fs from 'fs';
+import * as natural from 'natural';
 
 
 @Injectable()
@@ -12,6 +13,8 @@ export class NlpService {
     this.manager = new NlpManager({ languages: ['en'] });
   }
 
+ 
+
   public async trainModel(): Promise<void> {
     const data: any[] = [];
     fs.createReadStream('./trainingData.csv')
@@ -20,14 +23,49 @@ export class NlpService {
         data.push(row);
       })
       .on('end', () => {
+        const tokenizer = new natural.WordTokenizer();
+        const wordTokenizer = new natural.WordTokenizer();
+        const tfidf = new natural.TfIdf();
         for (const intent of data) {
-          this.manager.addDocument('en', intent.utterances, intent.intent);
+          const tokens = tokenizer.tokenize(intent.utterances);
+          tfidf.addDocument(tokens);
+          for (const word of tokens) {
+            const wordTokens = wordTokenizer.tokenize(word);
+            for (const token of wordTokens) {
+              this.manager.addDocument('en', token, intent.intent);
+            }
+          }
           this.manager.addAnswer('en', intent.intent, intent.answers);
         }
+        tfidf.listTerms(0).forEach((item) => {
+          this.manager.addNamedEntityText(
+            'en',
+            item.term,
+            'word_embedding',
+            [item.tfidf],
+          );
+        });
         this.manager.train();
         this.manager.save();
       });
   }
+
+  // public async trainModel(): Promise<void> {
+  //   const data: any[] = [];
+  //   fs.createReadStream('./trainingData.csv')
+  //     .pipe(csv())
+  //     .on('data', (row) => {
+  //       data.push(row);
+  //     })
+  //     .on('end', () => {
+  //       for (const intent of data) {
+  //         this.manager.addDocument('en', intent.utterances, intent.intent);
+  //         this.manager.addAnswer('en', intent.intent, intent.answers);
+  //       }
+  //       this.manager.train();
+  //       this.manager.save();
+  //     });
+  // }
 
   public async processIntent(text: string): Promise<string> {
     const result = await this.manager.process('en', text);
